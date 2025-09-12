3c9a8cb9e8aea1f9afd081f3deb1cfaaaa1bfa39
Kim-MyungJin <myungjin.kim@switchwon.com>
2025-09-12 18:48:07 +0900

    init project

---
+++ src/main/kotlin/org/mj/mansour/backend/MansourBackendApplication.kt
@@ -0,0 +1,11 @@
+package org.mj.mansour.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MansourBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MansourBackendApplication>(*args)
+}
---
+++ src/test/kotlin/org/mj/mansour/backend/MansourBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.mj.mansour.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MansourBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
