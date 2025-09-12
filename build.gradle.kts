96dffa82e1db0116eb4bba398ec3a240d6821179
Kim-MyungJin <myungjin.kim@switchwon.com>
2025-09-12 18:58:21 +0900

    lint 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,17 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
     id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
+    id("idea")
+}
+
+idea {
+    module {
+        isDownloadSources = true
+    }
 }
 
 group = "org.mj.mansour"
